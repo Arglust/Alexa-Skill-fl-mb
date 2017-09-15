ddbcacee626e61545dfa2f250db75cd472fd982a
Manti21 <31655276+Manti21@users.noreply.github.com>
2017-09-15 10:23:48 +0200

    Cleaning unused Code

--- src/main/alexa/skill/LastFMArtistSearch.java
+++ src/main/alexa/skill/LastFMArtistSearch.java
@@ -24,90 +24,16 @@ public class LastFMArtistSearch {
 	public LastFMArtistSearch(String artist) {
 		this.artist = artist;
 		ObjectMapper mapper = new ObjectMapper();
-		String queryUrl = getQueryStringInfoArtist(artist);
+		String queryUrl = getQueryStringGeoArtist(artist);
 
 		try {
 			obj = mapper.readValue(new URL(queryUrl), GeoArtist.class);
 		} catch (IOException e) {
 			// e.printStackTrace();
-			boolRetrieval = true;
+			//boolRetrieval = true;
 		}
-		this.artist = obj.artist.name;
 	}
 
-	/**
-	 * This Method uses the LASTFM Api to return a ArrayList containing the Similar
-	 * artists divided by comma
-	 * 
-	 * @author flo, mbeckert
-	 * @param artist
-	 * @return ArrayList with Strings
-	 */
-	public ArrayList<String> getSimilarArtistsList() {
-		ArrayList<String> similarArtistsList = new ArrayList<String>();
-		// If there is an Error with the Api Request, then the list will contain
-		// RETRIEVAL_FAILED
-		if (boolRetrieval) {
-			similarArtistsList.add(RETRIEVAL_FAILED);
-			return similarArtistsList;
-		}
-		for (Artist_ a : obj.artist.similar.artist) {
-			similarArtistsList.add(a.name);
-		}
-		return similarArtistsList;
-	}
-
-	/**
-	 * This Method checks if there is a tag like "german, deutsch, austrian etc." in
-	 * the tag list of last FM. If there is such an tag, then it is true.
-	 * 
-	 * @author mbeckert
-	 * @param artist
-	 * @return
-	 */
-
-	public Boolean checkGerman() {
-		for (Tag t : obj.artist.tags.tag) {
-			int i = 0;
-			while (i < germanTags.length) {
-				if (t.name.toLowerCase().contains((germanTags[i].toLowerCase()))) {
-					return true;
-				}
-				i++;
-			}
-		}
-		return false;
-	}
-
-	public String getArtistImageListURL(String type) {
-		String artistImageListURL = null;
-		for (Image i : obj.artist.image) {
-			if (type.equals(i.size)) {
-				artistImageListURL = i.text;
-			}
-		}
-		return artistImageListURL;
-	}
-
-	public String getArtistName() {
-		return this.artist;
-	}
-
-	// Not Used Anymore
-	// /**
-	// * This Method builds the LAST FM Query String for the api-method
-	// * "getsimilarartist"
-	// *
-	// * @author mbeckert
-	// * @param artist
-	// * @return String queryurl
-	// */
-	// private static String getQueryStringSimilarArtist(String artist) {
-	// String queryUrl = ROOT_URL + "method=artist.getsimilar" + "&limit=" + LIMIT +
-	// "&autocorrect=" + AUTOCORRECT
-	// + "&artist=" + artist + "&api_key=" + API_KEY + "&format=json";
-	// return queryUrl;
-	// }
 
 	/**
 	 * This Method builds the LAST FM Query String for the api-method
@@ -118,7 +44,7 @@ public class LastFMArtistSearch {
 	 * @return String queryurl
 	 */
 	@SuppressWarnings("deprecation")
-	private String getQueryStringInfoArtist(String artist) {
+	private String getQueryStringGeoArtist(String country) {
 		String queryUrl = ROOT_URL + "method=geo.gettopartists&country=" + country + "&limit=" + LIMIT + "&api_key="
 				+ API_KEY + "&format=json";
 		// Sometimes there is an error with spaces and the lastfm api
@@ -127,10 +53,6 @@ public class LastFMArtistSearch {
 	}
 	//
 	 public static void main(String[] args) {
-	 LastFM lastfm = new LastFM("Darkened Sun");
-	
-	 System.out.println("is German: " + lastfm.checkGerman());
-	 System.out.println("Similar Artists: " + lastfm.getSimilarArtistsList());
-	 System.out.println("image URL: "+lastfm.getArtistImageListURL("large"));
+	 GeoArtist geoArtist = new GeoArtist();
 	 }
 }
